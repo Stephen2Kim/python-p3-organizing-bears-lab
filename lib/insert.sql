3650d3c6ed0f6bcf3358b469c8c16a9184e31783
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-10-26 14:46:01 +0300

    created bear tables

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'friendly', 1),
+('Rowdy', 3, 'M', 'black', 'aggressive', 1),
+('Tabitha', 2, 'F', 'golden', 'playful', 1),
+('Sergeant Brown', 4, 'M', 'brown', 'grumpy', 1),
+('Melissa', 3, 'F', 'black', 'gentle', 1),
+('Grinch', 6, 'M', 'green', 'sneaky', 1),
+('Wendy', 2, 'F', 'white', 'curious', 1),
+(NULL, 1, 'M', 'gray', 'shy', 1);  -- unnamed bear
